7c84709b7a9a278952473ac16b549132db36037a
shidexiao <sswsdx@163.com>
2020-10-11 12:07:43 +0800

    django mysql migrate and  book app

--- django_vue/settings.py
+++ django_vue/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api_test',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'django_vue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_vue_db',
+        'USER': 'root',
+        'PASSWORD': 'root1234',
+        'HOST': '127.0.0.1',
     }
 }
 
--- django_vue/urls.py
+++ django_vue/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+import api_test.urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/', include(api_test.urls)),
+
 ]
